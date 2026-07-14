e7a5e52eb546f7c31be9dee59e775a564133a394
gauss <gao.yj86@gamil.com>
2026-07-14 22:41:34 +0800

    Move sidebar navigation to top

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1252,6 +1252,11 @@ export default function App() {
                 }}
               >
                 <div className="sidebar-scrollbar-scope flex h-full min-h-0 flex-col border-r border-border/60 bg-sidebar">
+                  <SidebarRail
+                    activeView={sidebarView}
+                    onSelectView={persistSidebarView}
+                    changedCount={sourceControl.changedCount}
+                  />
                   <div
                     key={sidebarView}
                     className="min-h-0 flex-1 terax-panel-in"
@@ -1281,11 +1286,6 @@ export default function App() {
                       />
                     )}
                   </div>
-                  <SidebarRail
-                    activeView={sidebarView}
-                    onSelectView={persistSidebarView}
-                    changedCount={sourceControl.changedCount}
-                  />
                 </div>
               </ResizablePanel>
               <ResizableHandle withHandle />
